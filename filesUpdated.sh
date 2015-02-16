f39ea3932790df3e24d571811137037d86e29e95
wrbeasl <wrbeasl@g.clemson.edu>
2015-02-16 11:34:28 -0500

    Almost done

--- filesUpdated.sh
+++ filesUpdated.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#####################################################################
+#					William Beasley									#
+#						wrbeasl										#
+#						CPSC 424									#
+#					Homework 2 Question #2							#
+#####################################################################
+
 containsElement () {
   local e
   for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
@@ -17,30 +24,56 @@ Starting_Directory=$1
 TargetTime=$2
 ScriptMode=$3
 index=0
-declare -A OwnerArray
-declare -A FileCount
+declare -A Array
+declare -A FileArray
+
+#####################################################################
+#					Check the mode type 							#
+#####################################################################
 
 if (( ScriptMode == 1 )); then
-	FILES="$(find $Starting_Directory -mtime $TargetTime)"
+	FILES="$(find $Starting_Directory -type f -mtime $TargetTime)"
+	shopt -s globstar
 
-	for f in "${FILES[@]}"; do
-		Owner=$(ls -ld $f | awk '{print $3}')
-		
-		containsElement $Owner "${OwnerArray[@]}"
-		if(($?==0)); then
-			OwnerArray["$Owner"]="$Owner"
+	for f in $FILES; do
+		Owner=$(ls -ld $f | awk '{printf $3}')
+		if [ ${Array[$Owner]+_} ]; then
+			((Array[$Owner]++))	
+		else
+			((Array[$Owner]=1))
 		fi
-		((FileCount["$Owner"]++))
-
-		printf "%s\n" "${OwnerArray[@]}"
-		printf "%s\n" "${FILES[@]}"
-		printf "%s\n" "${FileCount[@]}"
-
 	done
 
+	printf "Owner\tNumber of files changed\n"
 
+	for i in "${!Array[@]}"; 
+	do
+		printf "%s\t %s\n" "$i" "${Array[$i]}"
+	done | sort -rn -k3
 
 else
-	echo "Mode 2"
+	FILES="$(find $Starting_Directory -type f -mtime $TargetTime)"
+	shopt -s globstar
+
+	for f in $FILES; do
+		Owner=$(ls -ld $f | awk '{printf $3}')
+		if [ ${Array[$Owner]+_} ]; then
+			((Array[$Owner]++))	
+		else
+			((Array[$Owner]=1))
+		fi
+	done
+
+	for i in "${!Array[@]}"; 
+	do
+		printf "%s\t\n" "$i"
+		echo "$i"
+		USER_FILES="$(find $Starting_Directory -type f -user "$i" -mtime $TargetTime)"
+		for f in $USER_FILES;
+		do
+			ls -lt $f
+		done
+
+	done | sort -rn -k3
 fi
 
